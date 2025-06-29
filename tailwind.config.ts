54725dbea45de4c95213854d239e068df7f7e267
hyunjihub <syub98774@naver.com>
2025-06-29 13:44:25 +0900

    [Init] tailwindCSS 설정 및 fontFamily 설정, default CSS 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      pretendard: ['Pretendard'],
+      kkubulim: ['Kkubulim'],
+    },
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [scrollbarHide],
+} satisfies Config;
