436ae6184100f94d1db4a9fa1cbe073017499abe
hyunjihub <syub98774@naver.com>
2025-07-19 14:05:24 +0900

    [Feat] 비회원일 경우, 관광지 찜 버튼 및 동작 제거 / [Refactor] 오늘의 웰니스 지수 dynamic import 적용

--- src/app/(_components)/landing/SpotItem.tsx
+++ src/app/(_components)/landing/SpotItem.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import Image from 'next/image';
+import { isLoggedInAtom } from '@/app/(_store)/auth';
 import like from '/public/images/icons/like.svg';
 import noLike from '/public/images/icons/no-like.svg';
+import { useAtomValue } from 'jotai';
 import { useState } from 'react';
 
 export default function SpotItem() {
   const [isLiked, setIsLiked] = useState(false);
+  const isLoggedIn = useAtomValue(isLoggedInAtom);
 
   return (
     <li className="max-w-[102px] flex flex-col">
@@ -18,12 +21,15 @@ export default function SpotItem() {
       <div className="mt-[20px] flex justify-between items-center">
         <p className="text-xs text-g3 tracking-[-2%]">명상</p>
         <Image
-          className="cursor-pointer"
+          className={`cursor-pointer ${
+            isLoggedIn ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+          }`}
           src={isLiked ? like : noLike}
           alt="쉼표"
           width={24}
           height={24}
           onClick={() => setIsLiked(!isLiked)}
+          aria-hidden={!isLoggedIn}
         />
       </div>
       <p className="mt-[8px] font-semibold text-xs text-b3 tracking-[-2%] line-clamp-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image';
 import LikedSpot from './(_components)/landing/LikedSpot';
 import MainSlider from './(_components)/landing/MainSlider';
 import Recommend from './(_components)/landing/Recommend';
-import TodayWellness from './(_components)/landing/wellness/TodayWellness';
+import dynamic from 'next/dynamic';
 import logo from '/public/images/icons/logo.svg';
+
+const TodayWellness = dynamic(() => import('./(_components)/landing/wellness/TodayWellness'));
 
 export default function Home() {
   return (
