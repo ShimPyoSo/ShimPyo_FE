3bfe57b62b5b1c1b69ebbdf6d990d46c06a4daa5
hyunjihub <syub98774@naver.com>
2025-08-02 16:40:03 +0900

    [Fix] isLoggedIn 여부에 따라 Tabbar 마이페이지/로그인 구분

--- src/app/(_components)/UI/TabBar.tsx
+++ src/app/(_components)/UI/TabBar.tsx
@@ -3,6 +3,7 @@
 import TabItem from './TabItem';
 import home from '/public/images/icons/tabbar/home.svg';
 import homeActive from '/public/images/icons/tabbar/homeActive.svg';
+import { isLoggedInAtom } from '@/app/(_store)/auth';
 import mypage from '/public/images/icons/tabbar/mypage.svg';
 import mypageActive from '/public/images/icons/tabbar/mypageActive.svg';
 import { rendering } from '@/app/(_utils)/constants';
@@ -10,10 +11,12 @@ import search from '/public/images/icons/tabbar/search.svg';
 import searchActive from '/public/images/icons/tabbar/searchActive.svg';
 import test from '/public/images/icons/tabbar/test.svg';
 import testActive from '/public/images/icons/tabbar/testActive.svg';
+import { useAtomValue } from 'jotai';
 import { usePathname } from 'next/navigation';
 
 export default function TabBar() {
   const pathname = usePathname();
+  const isLoggedIn = useAtomValue(isLoggedInAtom);
 
   if (!rendering.includes(pathname) && !pathname.startsWith('/search')) return null;
 
@@ -36,11 +39,11 @@ export default function TabBar() {
           active={pathname.startsWith('/test')}
         />
         <TabItem
-          href="/mypage"
+          href={isLoggedIn ? '/mypage' : '/login'}
           icon={mypage}
           activeIcon={mypageActive}
-          label="마이페이지"
-          active={pathname.startsWith('/mypage')}
+          label={isLoggedIn ? '마이페이지' : '로그인'}
+          active={isLoggedIn ? pathname.startsWith('/mypage') : pathname.startsWith('/login')}
         />
       </ul>
     </nav>
