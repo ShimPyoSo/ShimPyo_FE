7c6a10c9b8af692dfe5fa4d946aee5a2c4d72161
hyunjihub <syub98774@naver.com>
2025-08-12 12:05:48 +0900

    [Fix] 집중률 예측 추이 서비스 키 변경

--- src/app/api/concentration/route.ts
+++ src/app/api/concentration/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
   // 관광지 집중률 호출 (현재 간현관광지 고정)
-  const url = `https://apis.data.go.kr/B551011/TatsCnctrRateService/tatsCnctrRatedList?serviceKey=${process.env.WEATHER_SERVICE_KEY}&pageNo=1&numOfRows=7&MobileOS=WEB&MobileApp=shimpyo&areaCd=51&signguCd=51130&tAtsNm=%EA%B0%84%ED%98%84%EA%B4%80%EA%B4%91%EC%A7%80&_type=json`;
+  const url = `https://apis.data.go.kr/B551011/TatsCnctrRateService/tatsCnctrRatedList?serviceKey=${process.env.CONCENTRATION_SERVICE_KEY}&pageNo=1&numOfRows=7&MobileOS=WEB&MobileApp=shimpyo&areaCd=51&signguCd=51130&tAtsNm=%EA%B0%84%ED%98%84%EA%B4%80%EA%B4%91%EC%A7%80&_type=json`;
   const res = await fetch(url);
   const data = await res.json();
 
