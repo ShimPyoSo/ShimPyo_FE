c06002783f1eb7739ace4aba49c62fb520fa6d94
hyunjihub <syub98774@naver.com>
2025-08-13 15:33:58 +0900

    [Design] 검색 결과 화면 design

---
+++ src/app/(_components)/search/NoResult.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import noResult from '/public/images/noResult.svg';
+
+export default function NoResult() {
+  return (
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-220px)]">
+      <Image className="mb-[24px]" src={noResult} alt="결과 없음" width={102} height={130} />
+      <p className="text-b3 font-semibold tracking-[-1.3%]">검색 결과가 존재하지 않아요</p>
+      <span className="mt-[8px] flex gap-[16px] tracking-[-2%] text-xs">
+        <p className="text-gn1 font-semibold">이런 검색어는 어때요?</p>
+        <ul className="text-g1 flex gap-[8px]">
+          {['템플스테이', '요가', '촌캉스', '스파', '비건'].map((keyword) => (
+            <li key={keyword}>
+              <Link href={`/search/${keyword}`}>{keyword}</Link>
+            </li>
+          ))}
+        </ul>
+      </span>
+    </div>
+  );
+}
--- src/app/(_routes)/search/[word]/page.tsx
+++ src/app/(_routes)/search/[word]/page.tsx
@@ -2,10 +2,10 @@
 
 import { useEffect, useState } from 'react';
 
-import Image from 'next/image';
-import Link from 'next/link';
+import Filter from '@/app/(_components)/category/Filter/Filter';
+import NoResult from '@/app/(_components)/search/NoResult';
 import SearchInput from '@/app/(_components)/search/SearchInput';
-import noResult from '/public/images/noResult.svg';
+import SpotListItem from '@/app/(_components)/category/SpotListItem';
 
 export default function SearchResult() {
   const [isActive, setIsActive] = useState(true);
@@ -16,22 +16,26 @@ export default function SearchResult() {
   }, []);
 
   return (
-    <div className="min-h-full bg-w1 px-[16px]">
-      <SearchInput isActive={isActive} />
-      <div className="flex flex-col justify-center items-center h-[calc(100vh-220px)]">
-        <Image className="mb-[24px]" src={noResult} alt="결과 없음" width={102} height={130} />
-        <p className="text-b3 font-semibold tracking-[-1.3%]">검색 결과가 존재하지 않아요</p>
-        <span className="mt-[8px] flex gap-[16px] tracking-[-2%] text-xs">
-          <p className="text-gn1 font-semibold">이런 검색어는 어때요?</p>
-          <ul className="text-g1 flex gap-[8px]">
-            {['템플스테이', '요가', '촌캉스', '스파', '비건'].map((keyword) => (
-              <li key={keyword}>
-                <Link href={`/search/${keyword}`}>{keyword}</Link>
-              </li>
-            ))}
-          </ul>
-        </span>
+    <div className="min-h-full bg-w1">
+      <div className="px-[16px]">
+        <SearchInput isActive={isActive} />
       </div>
+
+      {true && <Filter />}
+      {true ? (
+        <ul className="px-[16px] pb-[20px] flex-1 overflow-y-auto">
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+          <SpotListItem type="spot" />
+        </ul>
+      ) : (
+        <NoResult />
+      )}
     </div>
   );
 }
