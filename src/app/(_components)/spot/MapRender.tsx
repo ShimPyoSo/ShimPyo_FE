cc52da45640b102a00a17f5d27c1d4e8f82f8721
hyunjihub <syub98774@naver.com>
2025-08-18 23:18:49 +0900

    [Fix] 상세페이지 지도 렌더링 되지 않는 오류 수정

--- src/app/(_components)/spot/MapRender.tsx
+++ src/app/(_components)/spot/MapRender.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { useEffect, useState } from 'react';
 
 interface MapRenderProps {
   latitude: number;
@@ -8,6 +9,30 @@ interface MapRenderProps {
 }
 
 export default function MapRender({ latitude, longitude }: MapRenderProps) {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      setIsReady(true);
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_CLIENT_KAKAO_JS_KEY}&autoload=false`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => setIsReady(true));
+    };
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  if (!isReady) return null;
+
   return (
     <Map
       center={{ lat: latitude, lng: longitude }}
