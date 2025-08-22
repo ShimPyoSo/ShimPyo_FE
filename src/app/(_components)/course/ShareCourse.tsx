fcdcd8524646a8f70401f62373c427e816628f80
hyunjihub <syub98774@naver.com>
2025-08-22 21:47:00 +0900

    [Design] 코스 공유 페이지 화면 design

---
+++ src/app/(_components)/course/ShareCourse.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useParams, useSearchParams } from 'next/navigation';
+
+import CourseList from './CourseList';
+import { ICourse } from '@/app/(_utils)/type';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+interface ShareCourseProps {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function ShareCourse({ setIsOpen }: ShareCourseProps) {
+  const { id } = useParams();
+  const params = useSearchParams();
+  const token = params.get('token');
+
+  const fetchShareCourse = async () => {
+    try {
+      const response = await axios.get<ICourse>(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/course/share`, {
+        params: { courseId: id, token },
+      });
+      return response.data;
+    } catch {}
+  };
+
+  const { data } = useQuery({
+    queryKey: ['shareCourse', id, token],
+    queryFn: fetchShareCourse,
+    enabled: !!id && !!token,
+  });
+
+  return (
+    <div className="bg-w1 px-[16px] pb-[30px]">
+      {data && <CourseList isEditable={false} setIsOpen={setIsOpen} course={data} />}
+    </div>
+  );
+}
--- src/app/(_routes)/course/[id]/page.tsx
+++ src/app/(_routes)/course/[id]/page.tsx
@@ -1,3 +1,22 @@
+import { Suspense, useState } from 'react';
+
+import Alert from '@/app/(_components)/UI/Alert';
+import ShareCourse from '@/app/(_components)/course/ShareCourse';
+
 export default function CourseShare() {
-  return <div className="bg-w1 pl-[16px] pb-[70px]"></div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Suspense>
+      <ShareCourse setIsOpen={setIsOpen} />
+      {isOpen && (
+        <Alert
+          title="링크 복사"
+          description={'여행지 링크가 복사되었습니다.\n친구에게 공유해 보세요.'}
+          confirmText="확인"
+          setIsOpen={setIsOpen}
+          onConfirm={() => setIsOpen(false)}
+        />
+      )}
+    </Suspense>
+  );
 }
