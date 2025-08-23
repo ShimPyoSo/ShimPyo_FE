89a9f2a1f8190d3c167947e06497f1edb9738a7d
hyunjihub <syub98774@naver.com>
2025-08-23 15:22:22 +0900

    [Design] 후기 작성 실시간 글자수 추가

--- src/app/(_components)/category/review/ReviewInput.tsx
+++ src/app/(_components)/category/review/ReviewInput.tsx
@@ -8,12 +8,17 @@ export default function ReviewInput({ contents, setContents }: ReviewInputProps)
     <section className="mt-[30px] tracking-[-2%]">
       <p className="font-[kkubulim] text-lg text-gn1">후기 내용</p>
       <small className="text-sm text-g1">해당 여행지에 방문한 솔직한 후기를 작성해 주세요</small>
-      <textarea
-        className="mt-[16px] w-full h-[175px] p-[16px] rounded-lg bg-w3 border border-w4 text-xs text-b1 overflow-y-auto outline-none resize-none"
-        value={contents}
-        onChange={(e) => setContents(e.target.value)}
-        placeholder="최소 5자의 후기 내용을 입력해 주세요"
-      />
+      <div className="relative">
+        <textarea
+          className="mt-[16px] w-full h-[175px] p-[16px] rounded-lg bg-w3 border border-w4 text-xs text-b1 overflow-y-auto outline-none resize-none"
+          value={contents}
+          onChange={(e) => setContents(e.target.value)}
+          placeholder="최소 5자의 후기 내용을 입력해 주세요"
+        />
+        <span className="absolute bottom-[16px] right-[16px] flex items-center text-xs text-g3/80 tracking-[-2%]">
+          <p className="text-g1/80">{contents.length}</p>/500
+        </span>
+      </div>
     </section>
   );
 }
