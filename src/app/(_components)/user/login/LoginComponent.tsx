a84d0a43b5731813b0dde7896ec970656964d897
hyunjihub <syub98774@naver.com>
2025-09-04 14:33:17 +0900

    [Design] 로그인 페이지 로고 추가, 해상도에 따른 margin 조정

--- src/app/(_components)/user/login/LoginComponent.tsx
+++ src/app/(_components)/user/login/LoginComponent.tsx
@@ -1,18 +1,23 @@
+import Image from 'next/image';
 import KakaoButton from '../KakaoButton';
 import Link from 'next/link';
 import LoginForm from './LoginForm';
+import logo from '/public/images/loginLogo.svg';
 
 export default function LoginComponent() {
   return (
-    <div className="h-full bg-w1 px-[16px] pt-[30px] flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-w1 px-[16px] pt-[12px] lg:pt-[30px] flex flex-col justify-center items-center">
+      <Link href={'/'} className="mb-[24px] lg:mb-[40px]">
+        <Image src={logo} alt="로고" width={170} height={52} />
+      </Link>
       <LoginForm />
-      <div className="flex items-center mt-[85px] mb-[16px]">
+      <div className="w-full flex items-center mt-[40px] lg:mt-[85px] mb-[16px]">
         <div className="flex-grow border-t border-g3"></div>
         <p className="mx-[10px] text-xs text-g3 whitespace-nowrap">간편 로그인</p>
         <div className="flex-grow border-t border-g3"></div>
       </div>
       <KakaoButton type="login" />
-      <p className="mt-[80px] mb-[20px] text-center text-xs text-g1 tracking-[-0.02em]">
+      <p className="mt-[20px] lg:mt-[80px] mb-[20px] text-center text-xs text-g1 tracking-[-0.02em]">
         아직 회원이 아니신가요?{' '}
         <Link className="text-b1 underline" href={'/signup'}>
           회원가입
