453a99efa5f8cc61f3b650a5a0d7defcd461cf04
hyunjihub <syub98774@naver.com>
2025-09-04 16:22:45 +0900

    [Fix] 테스트 다음, 이전 문제로 이동 시 스크롤 위치 최상단으로 이동

--- src/app/(_components)/test/Question.tsx
+++ src/app/(_components)/test/Question.tsx
@@ -4,12 +4,19 @@ import Answer from './Answer';
 import Loader from '../UI/Loader';
 import Progress from './Progress';
 import useQuestion from '@/app/(_utils)/hooks/useTestQuestion';
+import { useRef } from 'react';
 
 export default function Question() {
+  const topRef = useRef<HTMLDivElement>(null);
   const { currentIndex, selected, setSelected, handleNext, handlePrev } = useQuestion();
+
+  const scrollToTop = () => {
+    topRef.current?.scrollIntoView({ behavior: 'instant', block: 'start' });
+  };
 
   return (
     <div className="grow-1 relative">
+      <div ref={topRef} /> {/* <- sentinel */}
       {currentIndex < 10 ? (
         <>
           <Progress num={currentIndex} />
@@ -17,14 +24,20 @@ export default function Question() {
           <div className="w-[343px] fixed bottom-[16px] flex items-center gap-[12px] tracking-[-0.013em]">
             <button
               className="grow py-[16px] border rounded-lg font-semibold bg-[#FBFBFB] text-b1 border-w4 outline-none"
-              onClick={handlePrev}
+              onClick={() => {
+                handlePrev();
+                scrollToTop();
+              }}
             >
               이전으로
             </button>
             <button
               disabled={selected === -1}
               className="grow py-[16px] border rounded-lg font-semibold bg-gn1 text-white border-gn5 outline-none"
-              onClick={handleNext}
+              onClick={() => {
+                handleNext();
+                scrollToTop();
+              }}
             >
               {currentIndex < 9 ? '다음으로' : '쉼표 유형 확인하기'}
             </button>
