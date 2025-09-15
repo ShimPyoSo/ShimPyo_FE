60442a305e8c2f8e22f5ee3ecbb56db8b0e27728
hyunjihub <syub98774@naver.com>
2025-09-15 12:01:35 +0900

    [Fix] 코스 추가 시 default 시간 적용, time input CSS 적용

--- src/app/(_components)/course/bottomSheet/CourseTime.tsx
+++ src/app/(_components)/course/bottomSheet/CourseTime.tsx
@@ -12,9 +12,10 @@ export default function CourseTime({ register }: CourseSpotContentProps) {
     <section className=" mb-[80px]">
       <p className="mt-[52px] tracking-[-0.02em] text-g1 text-xs">시간 선택</p>
       <input
-        className="mt-[12px] text-sm tracking-[-0.02em] text-b3 outline-none"
         type="time"
+        defaultValue="12:00"
         {...register('course.time', { required: '시간을 선택해주세요' })}
+        className="mt-[12px] rounded-xl border border-w4 bg-white px-3 py-2 text-sm tracking-[-0.02em] text-b3 outline-none"
       />
     </section>
   );
